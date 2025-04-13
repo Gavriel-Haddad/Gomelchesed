4f75bce8a181002af192e1980ab6946f743e83f2
ghaddad193 <ghaddad193@gmail.com>
2025-04-13 22:43:14 +0300

    test authentication

--- authentication.py
+++ authentication.py
@@ -1,16 +1,10 @@
 import streamlit as st
-import os
+import streamlit_authenticator as stauth
 
-def main():
-    # os.system(r"C:\Users\USER\AppData\Local\Microsoft\WindowsApps\PythonSoftwareFoundation.Python.3.10_qbz5n2kfra8p0\python.exe -m pip install --upgrade pip")
-    import pip
-    pip.main(["install", "streamlit-authenticator", "--force-reinstall"])
-    import streamlit_authenticator as stauth
-    
+def authenticate():
     # Load the credentials from st.secrets
     credentials = st.secrets["credentials"]
     cookie = st.secrets["cookie"]
-    preauthorized = st.secrets.get("preauthorized", {})
 
     # Create an instance of the Authenticate class
     authenticator = stauth.Authenticate(
@@ -18,17 +12,17 @@ def main():
         cookie["name"],      # cookie name
         cookie["key"],       # cookie key
         cookie["expiry_days"],
-        preauthorized  # optional preauthorized emails
     )
 
     # Create a login widget
     name, authentication_status, username = authenticator.login("Login", "main")
 
     if authentication_status is False:
-        st.error("Username/password is incorrect")
+        return False;
     elif authentication_status is None:
-        st.warning("Please enter your username and password")
+        return None
     else:
+        return True
         # If we're authenticated, show the protected content
         st.success(f"Welcome *{name}*!")
         st.write("You are now logged in. Here is your secure content...")
@@ -37,6 +31,3 @@ def main():
         if st.button("Logout"):
             authenticator.logout("Logout", "main")
             st.experimental_rerun()
-
-if __name__ == "__main__":
-    main()
