f5f6d5c5e9418b3074d4242658569590cf79ce6e
Gavriel Haddad <gavrihaddadwork@gmail.com>
2025-11-14 15:56:10 +0200

    WIP

--- main.py
+++ main.py
@@ -350,11 +350,6 @@ def handle_donation():
 
 
 def recombine_reciept_columns(df: pd.DataFrame):
-	split = df['קבלה'].astype(str).str.split('/', n=1, expand=True)
-	has_book = split[1].notna()
-	df['מספר פנקס'] = split[0].where(has_book, np.nan)
-	df['מספר קבלה'] = np.where(has_book, split[1], split[0])
-
 	cols = df.columns
 	idx = cols.get_loc('קבלה')
 
@@ -363,6 +358,12 @@ def recombine_reciept_columns(df: pd.DataFrame):
 
 	# Reorder and drop book_page
 	df = df[left + ['מספר פנקס', 'מספר קבלה'] + right]
+
+	split = df['קבלה'].astype(str).str.split('/', n=1, expand=True)
+	has_book = split[1].notna()
+	df['מספר פנקס'] = split[0].where(has_book, np.nan)
+	df['מספר קבלה'] = np.where(has_book, split[1], split[0])
+
 
 
 	return df
