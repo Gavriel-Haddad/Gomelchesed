c417a280e4b821a1ff1ec023a5d0c3054f5d0ccf
Gavriel Haddad <gavrihaddadwork@gmail.com>
2026-07-18 22:48:10 +0300

    added copy db script

--- data_access_layer.py
+++ data_access_layer.py
@@ -38,6 +38,7 @@ def load_purchases():
                                     """), engine.connect())
 
 def load_db():
+    # st.session_state["engine"] = sa.create_engine("sqlite:///local_db.sqlite")
     st.session_state["engine"] = sa.create_engine(st.secrets["postgres"]["db_url"], pool_pre_ping=True)
 
     load_days()
